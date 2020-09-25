dadc9f1aa73844566eb2d16a97702edfc3a11b3d
kokosowygosc <mati1995@onet.pl>
2020-09-25 19:04:57 +0200

    Added RestFulEncoderDecoder little task

--- simple_api/settings.py
+++ simple_api/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_ROOT, 'staticfiles'))
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.normpath(os.path.join(PROJECT_ROOT, 'static')),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
